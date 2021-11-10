75bbfb82dd1978314876f43a584739db5a8f2db7
silasmelo17 <silasmelo1967@gmail.com>
2021-11-10 19:53:21 -0300

    add theme provider

---
+++ src/theme.ts
@@ -0,0 +1,25 @@
+
+import { DefaultTheme } from 'styled-components'
+
+
+
+const defaultTheme: DefaultTheme = {
+    colors: {
+        primary: {
+            main: "#067BF9",
+            highlight: "#fff"
+        },
+        second: {
+            main: "#f13d4c",
+            highlight: "#fff"
+        },
+        third: {
+            main: "#efefef",
+            highlight: "#333"
+        },
+        success: "green",
+        error: "red"
+    },
+};
+
+export default defaultTheme;
